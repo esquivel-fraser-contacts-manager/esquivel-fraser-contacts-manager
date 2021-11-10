f04c46e8c7caf8026f5e8742cd25ede1703825c8
Cody Hastings <cody@hastings.email>
2021-11-10 09:24:42 -0600

    refactored if else if to be a switch statement. commented out previous code to go over refactor with bernie

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,49 @@ public interface Main {
 
     public static void main(String[] args) throws IOException {
 
+//        Boolean keepgoing = true;
+//        do{
+//            int userInput = ContactsController.mainMenu();
+//            if (userInput == 1){
+//                ContactsController.viewList();
+//            } else if (userInput == 2){
+//                ContactsController.addContact();
+//            } else if (userInput == 3){
+//                ContactsController.searchContact();
+//            } else if (userInput == 4){
+//                ContactsController.deleteContact();
+//            } else if (userInput == 5){
+//                ContactsController.exit();
+//                keepgoing = false;
+//            } else {
+//                System.err.println("Invalid Entry; enter a number from 1 to 5");
+//            }
+//
+//        }while(keepgoing);
+
         Boolean keepgoing = true;
         do{
             int userInput = ContactsController.mainMenu();
-            if (userInput == 1){
-                ContactsController.viewList();
-            } else if (userInput == 2){
-                ContactsController.addContact();
-            } else if (userInput == 3){
-                ContactsController.searchContact();
-            } else if (userInput == 4){
-                ContactsController.deleteContact();
-            } else if (userInput == 5){
-                ContactsController.exit();
-                keepgoing = false;
-            } else {
-                System.err.println("Invalid Entry; enter a number from 1 to 5");
+            switch (userInput){
+                case 1:
+                    ContactsController.viewList();
+                    break;
+                case 2:
+                    ContactsController.addContact();
+                    break;
+                case 3:
+                    ContactsController.searchContact();
+                    break;
+                case 4:
+                    ContactsController.deleteContact();
+                    break;
+                case 5:
+                    ContactsController.exit();
+                    keepgoing = false;
+                    break;
+                default:
+                    System.err.println("Invalid Entry; enter a number from 1 to 5");
             }
-
         }while(keepgoing);
     }
 }
